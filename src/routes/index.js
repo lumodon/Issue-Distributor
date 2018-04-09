5bd0877b5a51df6ae6d175a4e38fadb2bf7fe2ae
Serafin Wesnidge <leaf466@gmail.com>
2018-04-09 16:18:18 -0700

    Sets up test for db information

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ const router = require('express').Router()
 router.get('/', async (request, response) => {
   const csrs = await getCsrs() || [{ 'username': 'sample', 'issues': [101, 102] }]
 
-  response.render('index', csrs)
+  response.status(200).send(csrs)
+
+  // response.render('index', csrs)
 })
 
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const { getCsrs, setCsrs } = require('models')
+const { getCsrs, setCsrs, addIssue, clearIssues } = require('./models')
 require('dotenv').config()
 require('ejs')
 
